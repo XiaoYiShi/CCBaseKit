68138811dbd1650963beead1a2fc0ce96d717687
Yi.S <1270654114@qq.com>
2019-11-28 10:52:53 +0800

    数组扩展

--- CCBaseKit/CCExtension/CCExtension_Array.swift
+++ CCBaseKit/CCExtension/CCExtension_Array.swift
@@ -15,6 +15,72 @@ public extension Array where Element == String
         return self.joined(separator: ",")
     }
 }
+public extension Array
+{
+    /// 数组中第2个元素
+    var second: Element? {
+        if self.count > 1 {
+            return self[1]
+        }
+        return nil
+    }
+    /// 数组中第3个元素
+    var third: Element? {
+        if self.count > 2 {
+            return self[2]
+        }
+        return nil
+    }
+    /// 数组中第4个元素
+    var fourth: Element? {
+        if self.count > 3 {
+            return self[3]
+        }
+        return nil
+    }
+    /// 数组中第5个元素
+    var fifth: Element? {
+        if self.count > 4 {
+            return self[4]
+        }
+        return nil
+    }
+    /// 数组中第6个元素
+    var sixth: Element? {
+        if self.count > 5 {
+            return self[5]
+        }
+        return nil
+    }
+    /// 数组中第7个元素
+    var seventh: Element? {
+        if self.count > 6 {
+            return self[6]
+        }
+        return nil
+    }
+    /// 数组中第8个元素
+    var eighth: Element? {
+        if self.count > 7 {
+            return self[7]
+        }
+        return nil
+    }
+    /// 数组中第9个元素
+    var ninth: Element? {
+        if self.count > 8 {
+            return self[8]
+        }
+        return nil
+    }
+    /// 数组中第10个元素
+    var tenth: Element? {
+        if self.count > 9 {
+            return self[9]
+        }
+        return nil
+    }
+}
 
 
 //public extension Dictionary<Key, Value> where Key : String
