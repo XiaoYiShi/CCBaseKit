b16c9e23b049613e7a97b48d23b053d62bd32e4a
Yi.S <1270654114@qq.com>
2020-01-13 15:36:08 +0800

    array

--- CCBaseKit/CCExtension/CCExtension_Array.swift
+++ CCBaseKit/CCExtension/CCExtension_Array.swift
@@ -208,3 +208,181 @@ public extension Array where Element : NSObject
 //        }
 //    }
 //}
+
+
+// Provide some some common method for Array.
+public extension Array
+{
+//
+//    /**
+//     Creates and returns an array from a specified property list data.
+//
+//     @param plist   A property list data whose root object is an array.
+//     @return A new array created from the binary plist data, or nil if an error occurs.
+//     */
+//    static func arrayWithPlistData(_ plist:Data) -> [Element]?
+//    {
+//
+//        NSArray *array = [NSPropertyListSerialization propertyListWithData:plist options:NSPropertyListImmutable format:NULL error:NULL];
+//        if ([array is :[NSArray class]]) return array;
+//        return nil;
+//    }
+//
+//    /**
+//     Creates and returns an array from a specified property list xml string.
+//
+//     @param plist   A property list xml string whose root object is an array.
+//     @return A new array created from the plist string, or nil if an error occurs.
+//     */
+//    static func arrayWithPlistString(_ plist:String) -> [Element]?
+//    {
+//        guard let data = plist.data(using: String.Encoding.utf8) else {
+//            return nil
+//        }
+//        return arrayWithPlistData(data)
+//    }
+//
+//    /**
+//     Serialize the array to a binary property list data.
+//
+//     @return A binary plist data, or nil if an error occurs.
+//     */
+//    var plistData: Data {
+//        return [NSPropertyListSerialization dataWithPropertyList:self format:NSPropertyListBinaryFormat_v1_0 options:kNilOptions error:NULL];
+//    }
+//
+//
+//    /**
+//     Serialize the array to a xml property list string.
+//
+//     @return A plist xml string, or nil if an error occurs.
+//     */
+//    var plistString: String {
+//        NSData *xmlData = [NSPropertyListSerialization dataWithPropertyList:self format:NSPropertyListXMLFormat_v1_0 options:kNilOptions error:NULL];
+//        if (xmlData) return xmlData.utf8String;
+//        return nil;
+//    }
+//
+    /**
+     Returns the object located at a random index.
+
+     @return The object in the array with a random index value.
+     If the array is empty, returns nil.
+     */
+    var randomObject: Element? {
+        if self.count > 0 {
+            return self[Int(arc4random_uniform(UInt32(self.count)))]
+        }
+        return nil
+    }
+    
+    /**
+     Returns the object located at index, or return nil when out of bounds.
+     It's similar to `objectAtIndex:`, but it never throw exception.
+
+     @param index The object located at index.
+     */
+    func objectOrNil(at index:Int) -> Element? {
+        return index < self.count ? self[index] : nil
+    }
+    
+    /**
+     Convert object to json string. return nil if an error occurs.
+     NSString/NSNumber/NSDictionary/NSArray
+     */
+    var jsonStringEncoded: String?
+    {
+        if JSONSerialization.isValidJSONObject(self) {
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: self, options: JSONSerialization.WritingOptions.init(rawValue: 0))
+                return String.init(data: jsonData, encoding: String.Encoding.utf8)
+            } catch {
+            }
+        }
+        return nil
+    }
+    
+    /**
+     Convert object to json string formatted. return nil if an error occurs.
+     */
+    var jsonPrettyStringEncoded: String?
+    {
+        if JSONSerialization.isValidJSONObject(self) {
+            do {
+                let jsonData = try JSONSerialization.data(withJSONObject: self, options: JSONSerialization.WritingOptions.prettyPrinted)
+                return String.init(data: jsonData, encoding: String.Encoding.utf8)
+            } catch {
+            }
+        }
+        return nil
+    }
+}
+
+
+// Provide some some common method for mutating func.
+extension Array
+{
+    /**
+     Inserts a given object at the end of the array.
+
+     @param anObject The object to add to the end of the array's content.
+     This value must not be nil. Raises an NSInvalidArgumentException if anObject is nil.
+     */
+    mutating func addObject(_ anObject:Element) {
+        append(anObject)
+    }
+    
+    /**
+     Inserts a given object at the beginning of the array.
+
+     @param anObject The object to add to the end of the array's content.
+     This value must not be nil. Raises an NSInvalidArgumentException if anObject is nil.
+     */
+    mutating func prependObject(_ anObject:Element) {
+        insert(anObject, at: 0)
+    }
+    
+    /**
+     Adds the objects contained in another given array to the end of the receiving
+     array's content.
+
+     @param objects An array of objects to add to the end of the receiving array's
+     content. If the objects is empty or nil, this method has no effect.
+     */
+    mutating func addObjectsFromArray(_ objects:[Element]?) {
+        append(contentsOf: objects ?? [])
+    }
+    
+//    /**
+//     Adds the objects contained in another given array to the beginnin of the receiving
+//     array's content.
+//
+//     @param objects An array of objects to add to the beginning of the receiving array's
+//     content. If the objects is empty or nil, this method has no effect.
+//     */
+//    - (void)prependObjects:(NSArray *)objects {
+//        if (!objects) return;
+//        NSUInteger i = 0;
+//        for (id obj in objects) {
+//            [self insertObject:obj atIndex:i++];
+//        }
+//    }
+//
+//    /**
+//     Adds the objects contained in another given array at the index of the receiving
+//     array's content.
+//
+//     @param objects An array of objects to add to the receiving array's
+//     content. If the objects is empty or nil, this method has no effect.
+//
+//     @param index The index in the array at which to insert objects. This value must
+//     not be greater than the count of elements in the array. Raises an
+//     NSRangeException if index is greater than the number of elements in the array.
+//     */
+//    - (void)insertObjects:(NSArray *)objects atIndex:(NSUInteger)index {
+//        NSUInteger i = index;
+//        for (id obj in objects) {
+//            [self insertObject:obj atIndex:i++];
+//        }
+//    }
+}
